e9181ec77d0244f8c05c57a3d5b6231dc919b84f
vedaprada <65395472+vedaprada@users.noreply.github.com>
2020-06-25 17:23:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var i = 0;
+var txt = 'Open your eyes to'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector(".demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
+
+$(".top-container").mouseenter(function(){
+  $(".top-container-content").fadeOut("slow","swing").fadeIn("slow","swing");
+});
